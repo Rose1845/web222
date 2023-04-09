1945c8641e11d0798b18acb31b70fbb42138dd0e
Rose1845 <odhiamborose466@gmail.com>
2023-04-09 08:08:52 +0300

    ad review text and center it

--- data.js
+++ data.js
@@ -1,39 +1,39 @@
-// 5array onject for reviews
+// 5array onject for texts
 //include these 1) the name of the person
 // 2) the date
 // 3) a rating 1-5 (e.g., 3)
-// 4) the review of their review
+// 4) the text of their text
 
 window.reviewData = [
     {
         name: "John Doe",
         date: "2023-01-01",
         rating: 5,
-        review: "It's quite difficult to play"
+        text: "It's quite difficult to play"
     },
     {
         name: "Jane Doe",
         date: "2023-01-01",
         rating: 4,
-        review: "This is a good game for the mind!"
+        text: "This is a good game for the mind!"
     },
     {
         name: "John Smith",
         date: "2023-01-01",
         rating: 3,
-        review: "This is an ok product!"
+        text: "This is an ok product!"
     },
     {
         name: "Jane Smith",
         date: "2023-01-01",
         rating: 2,
-        review: "I need to be taught this game how can I get in touch with you!"
+        text: "I need to be taught this game how can I get in touch with you!"
     },
     {
         name: "Rose Odhiambo",
         date: "2023-01-01",
         rating: 3,
-        review: "This is a bad product!"
+        text: "This is a bad product!"
     },
 ];
     
--- review.js
+++ review.js
@@ -5,7 +5,7 @@
 function generateReviewCards() {
     const reviewContainer = document.getElementById("review-container");
     
-    reviewData.forEach((review) => {
+    reviewData.forEach((reviewD) => {
       // Create the card element
       console.log(review);
       const card = document.createElement("div");
@@ -13,33 +13,33 @@ function generateReviewCards() {
   
       // Create and append the card content
       const name = document.createElement("h3");
-      name.textContent = review.name;
+      name.textContent = reviewD.name;
       card.appendChild(name);
+
+      const text = document.createElement("p");
+      text.classList.add("review-text");
+      text.textContent = reviewD.text;
+      card.appendChild(text);
   
       const date = document.createElement("p");
       date.classList.add("review-date");
-      date.textContent = new Date(review.date).toLocaleDateString();
+      date.textContent = new Date(reviewD.date).toLocaleDateString();
       card.appendChild(date);
   
       const rating = document.createElement("p");
       rating.classList.add("review-rating");
     //   rating.textContent = "Rating: ";
-      for (let i = 0; i < review.rating; i++) {
+      for (let i = 0; i < reviewD.rating; i++) {
         rating.innerHTML += "★ ";
       }
-      for (let i = review.rating; i < 5; i++) {
+      for (let i = reviewD.rating; i < 5; i++) {
         rating.innerHTML += "☆ ";
       }
       card.appendChild(rating);
   
-      const text = document.createElement("p");
-      text.textContent = review.text;
-      card.appendChild(text);
-  
       // Append the card to the container
       reviewContainer.appendChild(card);
     //   form
-    
     });
   }
 generateReviewCards();
